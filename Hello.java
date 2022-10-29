bdd4096a842a466bc16ba44967551f693ef12383
cdacMahesh <114065292+cdacMahesh@users.noreply.github.com>
2022-10-29 18:59:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
 public static void main(String[] args) {
 System.out.println(" Welcome Hello world");
 System.out.println("cdac noida");
+System.out.println("ASDM Assignment 08");
 }
 }
